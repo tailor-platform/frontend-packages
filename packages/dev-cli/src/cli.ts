892ef78c247932260672f1c72cbd52fc2d7bdae3
IzumiSy <beetle-noise@gmx.com>
2023-12-27 17:39:42 +0900

    chore(dev-cli): use tailorctl v0.7.8

--- packages/dev-cli/src/cli.ts
+++ packages/dev-cli/src/cli.ts
@@ -40,7 +40,7 @@ export const runCLI = async (argv?: readonly string[]) => {
     .option(
       "--tailorctl-version <version>",
       "tailorctl version to download",
-      "v0.7.7"
+      "v0.7.8"
     )
     .option(
       "--cuelang-version <version>",
