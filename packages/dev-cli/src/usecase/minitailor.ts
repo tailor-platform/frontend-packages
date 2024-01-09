6b995598603a314bad5dc30d39f5394b97f80e55
IzumiSy <beetle-noise@gmx.com>
2024-01-09 18:48:47 +0900

    feat(dev-cli): add import command

---
+++ packages/dev-cli/src/usecase/minitailor.ts
@@ -0,0 +1,28 @@
+import chalk from "chalk";
+import { printError } from "../support/error.js";
+import { buildUsecase } from "../support/usecase.js";
+
+export type MinitailorRunOpts = {
+  paths: string[];
+  host: string;
+};
+
+export const importCmd = buildUsecase<MinitailorRunOpts>(
+  async ({ dockerCompose, args }) => {
+    try {
+      for (const path of args.paths) {
+        await dockerCompose.import(path, args.host, {
+          onRunning: (msg) => {
+            console.log(`${chalk.bold.yellow("[minitailor]")} ${msg}`);
+          },
+        });
+      }
+    } catch (e: unknown) {
+      if (e instanceof Object && "out" in e) {
+        printError(e.out);
+      } else {
+        printError(e);
+      }
+    }
+  }
+);
