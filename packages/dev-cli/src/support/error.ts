faede6c1aefc91adac6c66718adf488269ee70d4
IzumiSy <beetle-noise@gmx.com>
2024-01-11 18:59:41 +0900

    feat(dev-cli): fix error handling

--- packages/dev-cli/src/support/error.ts
+++ packages/dev-cli/src/support/error.ts
@@ -1,2 +1,10 @@
-export const printError = (e: unknown) =>
-  e instanceof Error ? console.error(`Failed: ${e.message}`) : console.error(e);
+export const printError = (e: unknown) => {
+  if (e instanceof Error) {
+    console.error(`Failed: ${e.message}`);
+  } else if (e instanceof Object && "out" in e) {
+    // This probably is Error object returned from docker-compose process
+    console.error(e.out);
+  } else {
+    console.error(e);
+  }
+};
