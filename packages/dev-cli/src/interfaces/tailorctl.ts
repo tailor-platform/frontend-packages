d0b62f935d5f6196a4d3202944329e3e32d49bbb
IzumiSy <beetle-noise@gmx.com>
2024-01-16 17:13:27 +0900

    feat(dev-cli): support tidy, apply, destroy, createWorkspace and createVault by tailorctl

--- packages/dev-cli/src/interfaces/tailorctl.ts
+++ packages/dev-cli/src/interfaces/tailorctl.ts
@@ -2,8 +2,45 @@ import { spawnExecutable, tailorctlBinary } from "../support/process.js";
 
 export type Tailorctl = {
   sync: () => Promise<number>;
+  tidy: () => Promise<number>;
+  apply: (manifest: string) => Promise<number>;
+  destroy: () => Promise<number>;
+  createWorkspace: (name: string) => Promise<number>;
+  createVault: () => Promise<number>;
 };
 
 export const cliTailorctlAdapter: Tailorctl = {
   sync: () => spawnExecutable(tailorctlBinary, ["cue", "sync"]),
+  tidy: () => spawnExecutable(tailorctlBinary, ["alpha", "manifest", "tidy"]),
+  apply: (manifest: string) =>
+    spawnExecutable(tailorctlBinary, [
+      "alpha",
+      "workspace",
+      "apply",
+      "-m",
+      manifest,
+    ]),
+  destroy: () =>
+    spawnExecutable(tailorctlBinary, [
+      "alpha",
+      "workspace",
+      "destroy",
+      "--auto-approve",
+    ]),
+  createWorkspace: (name: string) =>
+    spawnExecutable(tailorctlBinary, [
+      "alpha",
+      "workspace",
+      "create",
+      "--name",
+      name,
+    ]),
+  createVault: () =>
+    spawnExecutable(tailorctlBinary, [
+      "alpha",
+      "vault",
+      "create",
+      "--name",
+      "default",
+    ]),
 };
