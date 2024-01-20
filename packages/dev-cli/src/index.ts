faa95ba59b2d54e92ab375a6369a1ce66176b995
IzumiSy <beetle-noise@gmx.com>
2024-01-20 21:51:38 +0900

    chore(dev-cli): add tsup.config.ts

--- packages/dev-cli/src/index.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { runCLI } from "./cli/index.js";
-import { terminal } from "./cli/support/logger.js";
-
-runCLI(process.argv).catch((e) => {
-  terminal.error("app", e instanceof Error ? e.message : e);
-});
