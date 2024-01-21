9130d9b22143c13d7a9e21131f2ab2f8cef7526b
IzumiSy <beetle-noise@gmx.com>
2024-01-21 17:07:13 +0900

    chore(dev-cli): remove unnecessary args

--- packages/dev-cli/src/builtin/reset.ts
+++ packages/dev-cli/src/builtin/reset.ts
@@ -1,27 +1,10 @@
-import { cwd } from "process";
 import { fileIO } from "@builtin/internal/resource.js";
-import { defaultProfileName } from "@builtin/templates/compose.yaml.js";
-import { dockerCompose, getConfig, log } from "@script/index.js";
-
-const config = getConfig();
+import { dockerCompose, log } from "@script/index.js";
 
 await log.group("dev environment", "shutdown", async () => {
   const composePath = await fileIO.existsComposeConfig();
   if (composePath) {
-    const appName = config?.name || "";
-    await dockerCompose([
-      "-f",
-      composePath,
-      "--profile",
-      defaultProfileName,
-      "--project-directory",
-      cwd(),
-      "-p",
-      appName,
-      "down",
-      "--remove-orphans",
-      "--volumes",
-    ]);
+    await dockerCompose(["down", "--remove-orphans", "--volumes"]);
   }
 });
 
