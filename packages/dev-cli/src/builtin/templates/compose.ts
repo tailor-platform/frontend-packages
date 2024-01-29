54f63da5f71d8d489e1f90f7dc86539b6995730a
IzumiSy <beetle-noise@gmx.com>
2024-01-29 16:25:36 +0900

    chore(update): minitailor image version

--- packages/dev-cli/src/builtin/templates/compose.ts
+++ packages/dev-cli/src/builtin/templates/compose.ts
@@ -9,7 +9,7 @@ type InternalOptions = {
 type Options = DockerComposeOptions & InternalOptions;
 export const defaultDockerComposeOptions: Options = {
   // Specify "latest" here to use the latest minitailor image
-  minitailorImage: "2024.01.25-1",
+  minitailorImage: "2024.01.29-0",
   pullPolicy: "missing",
   profile: "app",
 };
