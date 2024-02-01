2e6d1d03b986dbd08ea2b449b3ba784ecaa69a20
IzumiSy <beetle-noise@gmx.com>
2024-02-01 17:19:12 +0900

    chore(oidc-client): fix tests

--- packages/oidc-client/src/lib/core.test.ts
+++ packages/oidc-client/src/lib/core.test.ts
@@ -1,20 +1,28 @@
 import { test, describe, expect } from "vitest";
 import { internalExchangeTokenForSession } from "./core";
+import { Session } from "./types";
 import { mockAuthConfig } from "@tests/config";
 
 describe("core", () => {
   test("internalExchangeTokenForSession", async () => {
-    const sessionResult = await internalExchangeTokenForSession(
+    const mockSession: Session = {
+      access_token: "mockAccessToken",
+      refresh_token: "mockRefreshToken",
+      expires_in: 86400,
+      user_id: "43a05b99-ebe1-4b89-8284-e4447e3a3551",
+    };
+    const fetchSpy = vi
+      .spyOn(global, "fetch")
+      .mockResolvedValue(new Response(JSON.stringify(mockSession)));
+
+    const sessionResult = (await internalExchangeTokenForSession(
       mockAuthConfig,
       "mockCode",
-    );
+    )) as Session;
 
-    expect(sessionResult).toHaveProperty("accessToken");
-    if ("accessToken" in sessionResult) {
-      expect(sessionResult.accessToken).toBe("mockAccessToken");
-    }
-    if ("refreshToken" in sessionResult) {
-      expect(sessionResult.refreshToken).toBe("mockRefreshToken");
-    }
+    expect(sessionResult.access_token).toBe("mockAccessToken");
+    expect(sessionResult.refresh_token).toBe("mockRefreshToken");
+
+    fetchSpy.mockRestore();
   });
 });
