03940b91492f48fb681fd2e7c7aafdec0385c88b
IzumiSy <beetle-noise@gmx.com>
2024-02-01 15:43:20 +0900

    feat(oidc-client): add error callback handler

--- packages/oidc-client/src/server/middleware.ts
+++ packages/oidc-client/src/server/middleware.ts
@@ -9,8 +9,18 @@ import { Session } from "@/lib/types";
 import { internalExchangeTokenForSession } from "@lib/core";
 
 type Options = {
-  prepend: (args: { token: string; userID: string }) => Promise<void> | void;
+  prepend?: (args: { token: string; userID: string }) => Promise<void> | void;
+  onError?: (err: Error) => Promise<void> | void;
 };
+
+class CallbackError extends Error {
+  constructor(
+    readonly type: string,
+    readonly message: string,
+  ) {
+    super(message);
+  }
+}
 
 export const withAuth = (
   config: Config,
@@ -20,29 +30,37 @@ export const withAuth = (
   return async (request, event) => {
     const nextURL = request.nextUrl;
     if (nextURL.pathname.startsWith(config.loginCallbackPath())) {
-      const code = nextURL.searchParams.get("code");
-      const redirectURI = nextURL.searchParams.get("redirect_uri");
-      if (!code || !redirectURI) {
-        // TODO: passing error reason with error code
-        return NextResponse.redirect(config.appUrl(config.unauthorizedPath()));
-      }
+      try {
+        const code = nextURL.searchParams.get("code");
+        const redirectURI = nextURL.searchParams.get("redirect_uri");
+        if (!code || !redirectURI) {
+          throw new CallbackError(
+            "E00000",
+            "code and redirectURI should be filled",
+          );
+        }
 
-      const session = await internalExchangeTokenForSession(config, code);
-      if ("error" in session) {
-        // TODO: passing error reason with error code
-        return NextResponse.redirect(config.appUrl(config.unauthorizedPath()));
-      }
-      options?.prepend &&
-        (await options.prepend({
-          token: session.access_token,
-          userID: session.user_id,
-        }));
+        const session = await internalExchangeTokenForSession(config, code);
+        if ("error" in session) {
+          throw new CallbackError("E00001", session.error);
+        }
+        options?.prepend &&
+          (await options.prepend({
+            token: session.access_token,
+            userID: session.user_id,
+          }));
 
-      const redirection = NextResponse.redirect(config.appUrl(redirectURI));
-      redirection.cookies.set(
-        buildCookieEntry(session, "tailor.token", "access_token"),
-      );
-      return redirection;
+        const redirection = NextResponse.redirect(config.appUrl(redirectURI));
+        redirection.cookies.set(
+          buildCookieEntry(session, "tailor.token", "access_token"),
+        );
+        return redirection;
+      } catch (e: unknown) {
+        if (e instanceof Error && options?.onError) {
+          return await options.onError(e);
+        }
+        // Here lets it fallback into running the next middleware in the end...
+      }
     } else if (nextURL.pathname.startsWith(clientSessionPath)) {
       const tailorToken = request.cookies.get("tailor.token");
       return NextResponse.json({ token: tailorToken?.value });
