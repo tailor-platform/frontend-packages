3a278d667506782bb26fd851bf12ab3dca05a874
IzumiSy <beetle-noise@gmx.com>
2024-02-02 14:37:32 +0900

    chore(oidc-client): update comments

--- packages/oidc-client/src/lib/config.ts
+++ packages/oidc-client/src/lib/config.ts
@@ -49,5 +49,12 @@ export class Config {
   }
 }
 
+// Internal path to fetch token from client components
+// `useSession` hook fetches token from this endpoint by extracting it from cookies.
 export const clientSessionPath = "/__auth/session" as const;
+
+// Internal path redirecting (through middleware) to `unauthorizedPath` set in ContextConfig
+// This is the path used in redirection caused by login guard in server components
+// The middleware that reads ContextConfig is in charge of redirecting to the `unauthorizedPath`
+// through this path when unauthorized users guarded in `getServerSession`.
 export const internalUnauthorizedPath = "/__auth/unauthorized" as const;
