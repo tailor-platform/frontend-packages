d47854685e5c429a83c39f6bb8325729e9f3e5ce
IzumiSy <beetle-noise@gmx.com>
2024-02-03 23:35:44 +0900

    feat(minitailorctl): initial version

---
+++ packages/minitailorctl/src/commands.ts
@@ -0,0 +1,37 @@
+import { createRequire } from "node:module";
+import { LevelledLogger } from "./logger.js";
+
+export const runBuiltinCommands = async (args: readonly string[]) => {
+  const { Command } = await import("@commander-js/extra-typings");
+  const program = new Command();
+
+  program.configureHelp({
+    sortSubcommands: true,
+    sortOptions: true,
+  });
+
+  const require = createRequire(import.meta.url);
+  const { version, description } = require("../package.json") as {
+    version: string;
+    description: string;
+  };
+  const logger = new LevelledLogger();
+  const app = program
+    .name("tailordev")
+    .description(description)
+    .option("--verbose", "enable verbosity", false)
+    .version(version)
+    .hook("preAction", (options) => {
+      const opts = options.opts();
+      if (opts.verbose) {
+        logger.setLevel("debug");
+        logger.debug("CLI", "enabled verbose mode");
+      }
+    });
+
+  //
+  // More builtin commands will be implemented here...
+  //
+
+  app.parse(args);
+};
