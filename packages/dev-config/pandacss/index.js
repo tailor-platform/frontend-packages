4471f9855029f15feb9411ba5ef5de9590164385
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-07 15:22:20 +0900

    feat(dc): add pandacss

---
+++ packages/dev-config/pandacss/index.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "@pandacss/dev";
+
+import {
+  conditions,
+  globalCss,
+  recipes,
+  semanticTokens,
+  slotRecipes,
+  textStyles,
+  tokens,
+} from "./../../design-systems/dist/client";
+
+export default defineConfig({
+  preflight: true,
+  include: [
+    "./src/stories/**/*.{js,jsx,ts,tsx}",
+    "./../../packages/design-systems/src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@tailor-platform/datagrid/dist/panda.buildinfo.json",
+    "./node_modules/@tailor-platform/design-systems/dist/panda.buildinfo.json",
+  ],
+  exclude: [],
+  jsxFramework: "react",
+  theme: {
+    extend: {
+      recipes,
+      slotRecipes,
+      semanticTokens,
+      textStyles,
+      tokens,
+    },
+  },
+  conditions,
+  globalCss,
+  emitPackage: true,
+  outdir: "@tailor-platform/styled-system",
+  importMap: "@tailor-platform/styled-system",
+});
