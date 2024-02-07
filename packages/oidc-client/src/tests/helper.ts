355612fc0f603465c0f423741d0ba88a68755bf7
IzumiSy <beetle-noise@gmx.com>
2024-02-07 21:29:37 +0900

    chore(oidc-client): add test helper to mock window.location.replace

---
+++ packages/oidc-client/src/tests/helper.ts
@@ -0,0 +1,17 @@
+import { Mock } from "vitest";
+
+// we can't simply spy on the window.location.replace method.
+// this is a helper function to completely replace the window.location object.
+export const withMockedReplace = async (
+  replaceMock: Mock,
+  test: () => Promise<void> | void,
+) => {
+  const originalWindowLocation = window.location;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+  delete (window as any).location;
+  window.location = { ...originalWindowLocation, replace: replaceMock };
+
+  await test();
+
+  window.location = originalWindowLocation;
+};
