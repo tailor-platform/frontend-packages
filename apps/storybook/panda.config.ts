7a23545e0a3a4567a92fd5e7a5f4005f07599648
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-07 15:22:20 +0900

    feat(dc): add pandacss

--- apps/storybook/panda.config.ts
+++ apps/storybook/panda.config.ts
@@ -1,37 +1,3 @@
-import { defineConfig } from "@pandacss/dev";
+import dsPandaConfig from "./node_modules/@tailor-platform/dev-config/pandacss";
 
-import {
-  conditions,
-  globalCss,
-  recipes,
-  semanticTokens,
-  slotRecipes,
-  textStyles,
-  tokens,
-} from "@tailor-platform/design-systems/client";
-
-export default defineConfig({
-  preflight: true,
-  include: [
-    "./src/stories/**/*.{js,jsx,ts,tsx}",
-    "./../../packages/design-systems/src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@tailor-platform/datagrid/dist/panda.buildinfo.json",
-    "./node_modules/@tailor-platform/design-systems/dist/panda.buildinfo.json",
-  ],
-  exclude: [],
-  jsxFramework: "react",
-  theme: {
-    extend: {
-      recipes,
-      slotRecipes,
-      semanticTokens,
-      textStyles,
-      tokens,
-    },
-  },
-  conditions,
-  globalCss,
-  emitPackage: true,
-  outdir: "@tailor-platform/styled-system",
-  importMap: "@tailor-platform/styled-system",
-});
+export default dsPandaConfig;
