58a8871642ccf0d0c15e3309a74e3c1218832807
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-07 17:06:20 +0900

    feat(dc): add function to create pandacss config

--- packages/dev-config/pandacss/index.js
+++ packages/dev-config/pandacss/index.js
@@ -8,9 +8,40 @@ import {
   slotRecipes,
   textStyles,
   tokens,
-} from "./../../design-systems/dist/client";
+} from "../../design-systems/dist/client";
 
-export default defineConfig({
+export function buildPandaConfig(config) {
+  const designSysetemConfig = {
+    preflight: true,
+    include: [
+      "./src/stories/**/*.{js,jsx,ts,tsx}",
+      "./../../packages/design-systems/src/**/*.{js,jsx,ts,tsx}",
+      "./node_modules/@tailor-platform/datagrid/dist/panda.buildinfo.json",
+      "./node_modules/@tailor-platform/design-systems/dist/panda.buildinfo.json",
+    ],
+    exclude: [],
+    jsxFramework: "react",
+    theme: {
+      recipes,
+      slotRecipes,
+      semanticTokens,
+      textStyles,
+      tokens,
+    },
+    conditions,
+    globalCss,
+    emitPackage: true,
+    outdir: "@tailor-platform/styled-system",
+    importMap: "@tailor-platform/styled-system",
+  };
+
+  return defineConfig({
+    ...designSysetemConfig,
+    ...config,
+  });
+}
+
+export const defaultPandaConfig = defineConfig({
   preflight: true,
   include: [
     "./src/stories/**/*.{js,jsx,ts,tsx}",
