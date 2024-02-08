a9f6ec39837dda7dfc0a678ce1348989f2184b15
IzumiSy <beetle-noise@gmx.com>
2024-02-08 11:35:14 +0900

    chore(oidc-client): add more tests

--- packages/oidc-client/src/server/middleware.test.ts
+++ packages/oidc-client/src/server/middleware.test.ts
@@ -1,15 +1,13 @@
 import { describe, expect, it } from "vitest";
 import { middlewareRouter } from "./middleware";
 import { mockAuthConfig } from "@tests/mocks";
-import {
-  buildRequestWithParams,
-  testingError,
-  testingErrorHandler,
-} from "@tests/helper";
+import { buildRequestWithParams } from "@tests/helper";
 
 describe("middleware", () => {
+  const testingError = new Error("this is testing error");
+
   describe("middlewareRouter", () => {
-    it("calls onError when an error occured in extra middleware", () => {
+    it("calls onError when an error occured in middleware handlers", () => {
       const errorPath = "/error-route";
       const defaultParams = {
         request: buildRequestWithParams(mockAuthConfig.appUrl(errorPath)),
@@ -26,11 +24,69 @@ describe("middleware", () => {
             },
           },
           {
-            [errorPath]: testingErrorHandler,
+            [errorPath]: () => {
+              throw testingError;
+            },
           },
         ),
       ).not.toThrowError();
       expect(onErrorMock).toHaveBeenCalledWith(testingError);
+    });
+
+    it("calls onError when an error occured in fallback middleware", () => {
+      const defaultParams = {
+        request: buildRequestWithParams(
+          mockAuthConfig.appUrl("/path-that-nothing-matches"),
+        ),
+        config: mockAuthConfig,
+      };
+      const onErrorMock = vi.fn();
+      const mockHandler = vi.fn();
+
+      expect(() =>
+        middlewareRouter(
+          {
+            ...defaultParams,
+            options: {
+              onError: onErrorMock,
+            },
+          },
+          {
+            "/mock-handler": mockHandler,
+          },
+          () => {
+            throw testingError;
+          },
+        ),
+      ).not.toThrowError();
+      expect(onErrorMock).toHaveBeenCalledWith(testingError);
+      expect(mockHandler).not.toHaveBeenCalled();
+    });
+
+    it("runs fallback middleware if no route matches", () => {
+      const request = buildRequestWithParams(
+        mockAuthConfig.appUrl("/test-path"),
+      );
+      const defaultParams = {
+        request,
+        config: mockAuthConfig,
+      };
+      const mockFallback = vi.fn();
+      const mockHandler = vi.fn();
+
+      expect(() =>
+        middlewareRouter(
+          {
+            ...defaultParams,
+          },
+          {
+            "/path-that-does-not-match": mockHandler,
+          },
+          mockFallback,
+        ),
+      ).not.toThrowError();
+      expect(mockFallback).toHaveBeenCalled();
+      expect(mockHandler).not.toHaveBeenCalled();
     });
   });
 });
