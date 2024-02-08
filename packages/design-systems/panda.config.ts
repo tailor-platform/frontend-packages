eed3537a6902c82ea578d4d4354e4e0980d96ac6
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-08 09:03:17 +0900

    fix: fix panda config

--- packages/design-systems/panda.config.ts
+++ packages/design-systems/panda.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-  include: [
-    "../**/*.{ts,tsx}",
-    "../../../node_modules/@tailor-platform/src/**/*/tsx",
-  ],
+  include: ["../**/*.{ts,tsx}"],
 });
