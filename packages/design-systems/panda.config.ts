5acc40879d175646dcdd54e2b4e3d4c66fd0f534
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-08 13:04:06 +0900

    fix: fix pandaconfig

--- packages/design-systems/panda.config.ts
+++ packages/design-systems/panda.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from "@pandacss/dev";
 
+import { recipes } from "./src/theme/recipes";
+import { semanticTokens } from "./src/theme/semantic-tokens";
+import { slotRecipes } from "./src/theme/slot-recipes";
+import { textStyles } from "./src/theme/text-styles";
+import { tokens } from "./src/theme/tokens";
+
 export default defineConfig({
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      recipes,
+      semanticTokens,
+      slotRecipes,
+      textStyles,
+      tokens,
+    },
+  },
 });
