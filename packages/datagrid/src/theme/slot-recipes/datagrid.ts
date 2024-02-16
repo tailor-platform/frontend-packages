97bfcc89f632b31c36d6428c020c9a83c3a1824d
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-02-16 11:05:03 +0900

    feat(dg): add slotrecipes wip

---
+++ packages/datagrid/src/theme/slot-recipes/datagrid.ts
@@ -0,0 +1,8 @@
+import { defineSlotRecipe } from "@pandacss/dev";
+
+export const datagrid = defineSlotRecipe({
+    className: "datagrid",
+    slots:[],
+    description: "An datagrid style",
+    base: {}
+})
