c95af15c7f13b35f303f25932cefb046f485669a
IzumiSy <beetle-noise@gmx.com>
2024-02-20 15:36:57 +0900

    chore(oidc-client): add more comments

--- packages/oidc-client/src/strategies/abstract.ts
+++ packages/oidc-client/src/strategies/abstract.ts
@@ -1,9 +1,13 @@
 import { Config } from "@lib/config";
 
 type CallbackResult = {
+  // Payload to be sent out to /auth/token endpoint on Tailor Platform to issue token
   payload: FormData;
+
+  // An URI to be redirected after callback handler
   redirectUri: string;
 };
+
 type AuthenticateResult =
   | {
       // Redirection-base authentication flow (eg. OIDC/SAML/...)
@@ -13,13 +17,14 @@ type AuthenticateResult =
       uri: string;
     }
   | {
-      // Manually calling callback
+      // This mode will manually send out the payload above to middleware callback handlers.
       mode: "manual-callback";
 
       // Payload to be sent to callback handler
       payload: FormData;
     };
 
+// AbstractStrategy is an interface that all authentication strategies are expected to implement
 export type AbstractStrategy<
   T extends Record<string, unknown> = Record<string, unknown>,
 > = {
