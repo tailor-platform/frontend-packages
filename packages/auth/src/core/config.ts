7afaa408554fdf938cab5d1b39b4efeb1926eb53
IzumiSy <beetle-noise@gmx.com>
2024-02-22 17:15:34 +0900

    chore(auth): add comment on defaultStrategy

--- packages/auth/src/core/config.ts
+++ packages/auth/src/core/config.ts
@@ -12,8 +12,9 @@ type ContextConfig = {
   userInfoPath: string;
 };
 
-// Default strategy is OIDC
-const defaultStrategy = new (class extends OIDCStrategy {
+// `defaultStrategy` is a strategy used if users don't specify the strategy name in `login` function
+// Currently, the default is OIDC, but fine to be changed in need.
+export const defaultStrategy = new (class extends OIDCStrategy {
   name() {
     return "default";
   }
