5f3d7518b85b84b3759da67211be5bb0e302d568
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-03-01 10:25:04 +0900

    feat(ds): add NumberInput component

--- packages/design-systems/src/index.tsx
+++ packages/design-systems/src/index.tsx
@@ -68,3 +68,8 @@ export {
   type DatePickerProps,
 } from "@/components/composite/DatePicker";
 export { Dialog, type DialogProps } from "@/components/composite/Dialog";
+
+export {
+  NumberInput,
+  type NumberInputProps,
+} from "@/components/composite/NumberInput";
