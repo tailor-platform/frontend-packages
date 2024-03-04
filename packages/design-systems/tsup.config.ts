9768df08de5007bda9257b84d48a9a4e543cef57
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-03-04 13:39:01 +0900

    fix(ds): combine tsup config files

--- packages/design-systems/tsup.config.ts
+++ packages/design-systems/tsup.config.ts
@@ -1,19 +1,31 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  banner: {
-    js: `"use client"`,
+export default defineConfig([
+  {
+    entry: {
+      index: "src/index.tsx",
+      "pandacss/index": "./src/pandacss/index.ts",
+      "locales/index": "src/locales/index.ts",
+      "locales/ja": "src/locales/ja.ts",
+      "locales/en": "src/locales/en.ts",
+    },
+    format: ["cjs", "esm"],
+    external: ["react"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
   },
-  entry: {
-    index: "src/index.tsx",
-    "pandacss/index": "./src/pandacss/index.ts",
-    "locales/index": "src/locales/index.ts",
-    "locales/ja": "src/locales/ja.ts",
-    "locales/en": "src/locales/en.ts",
+  {
+    banner: {
+      js: `"use client"`,
+    },
+    entry: {
+      client: "src/client.tsx",
+    },
+    format: ["cjs", "esm"],
+    external: ["react"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
   },
-  format: ["cjs", "esm"],
-  external: ["react"],
-  dts: true,
-  clean: true,
-  sourcemap: true,
-});
+]);
