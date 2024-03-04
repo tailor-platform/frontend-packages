fa8c7e04fe20c414895e5a5819e1a761a8d3327d
Ryosuke Murayama <ryosuke_murayama@MacBook-Air.local>
2024-03-04 10:52:01 +0900

    fix(ds): split config for use client

--- packages/design-systems/tsup-client.config.ts
+++ packages/design-systems/tsup-client.config.ts
@@ -5,8 +5,10 @@ export default defineConfig({
     js: `"use client"`,
   },
   entry: {
-    index: "src/index.tsx",
     client: "src/client.tsx",
+    "locales/index": "src/locales/index.ts",
+    "locales/ja": "src/locales/ja.ts",
+    "locales/en": "src/locales/en.ts",
   },
   format: ["cjs", "esm"],
   external: ["react"],
--- packages/design-systems/tsup.config.ts
+++ packages/design-systems/tsup.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: {
-    // index: "src/index.tsx",
+    index: "src/index.tsx",
     // client: "src/client.tsx",
     "pandacss/index": "./src/pandacss/index.ts",
-    "locales/index": "src/locales/index.ts",
-    "locales/ja": "src/locales/ja.ts",
-    "locales/en": "src/locales/en.ts",
+    // "locales/index": "src/locales/index.ts",
+    // "locales/ja": "src/locales/ja.ts",
+    // "locales/en": "src/locales/en.ts",
   },
   format: ["cjs", "esm"],
   external: ["react"],
