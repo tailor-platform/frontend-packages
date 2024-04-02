396e8a0605134913109f2bee6f62f7828d5bf1c5
IzumiSy <beetle-noise@gmx.com>
2024-04-02 18:25:25 +0900

    chore(auth): add module docs

--- packages/auth/src/client/index.ts
+++ packages/auth/src/client/index.ts
@@ -1,2 +1,8 @@
+/**
+ * `@tailor-platform/auth/client` is a package that provides a set of hooks and components
+ * to handle user authentication in a standardized manner on client components.
+ *
+ * @packageDocumentation
+ */
 export { TailorAuthProvider } from "@client/provider";
 export { useAuth, usePlatform, useSession } from "@client/hooks";
