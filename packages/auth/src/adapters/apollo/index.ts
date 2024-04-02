d0dcdf3cfa6f30656d66c5ebbff17ca0f3b507d9
IzumiSy <beetle-noise@gmx.com>
2024-04-02 18:33:15 +0900

    chore(auth): updated comments

--- packages/auth/src/adapters/apollo/index.ts
+++ packages/auth/src/adapters/apollo/index.ts
@@ -1,1 +1,6 @@
+/**
+ * `@tailor-platform/auth/adapters/apollo` is a package with custom ApolloLink that automatically sets tokens in authorization header as a bearer token.
+ *
+ * @packageDocumentation
+ */
 export { authenticatedHttpLink } from "./link";
