5cd293003bfd3d701539b056a10b04db838983d7
Yuuki_takahashi <highbridge71@gmail.com>
2024-05-16 14:44:26 +0900

    fix(ds): fix Datagrid component on modals not working (#201)
    * fix(ds): add DataGridOnModalWithFilterStory
    
    * refactor(ds): make ModalPortal component
    
    * refactor(ds): move ModalPortal
    
    * fix(ds): don't use ModalPortal
    
    * fix(ds): change z-index of select based on modal use
    
    * fix(ds): adjust display position based on modal use
    
    * refactor(ds): fix DataGridOnModalWithFilterStory
    
    * fix(ds): update version

--- packages/design-systems/src/components/composite/Datagrid/ColumnFeature/PinnedColumn.tsx
+++ packages/design-systems/src/components/composite/Datagrid/ColumnFeature/PinnedColumn.tsx
@@ -1,26 +1,11 @@
-import { useState, useRef, useEffect, CSSProperties } from "react";
+import { useState, useRef, CSSProperties } from "react";
 import { Box } from "@tailor-platform/styled-system/jsx";
 import type { Column } from "@tanstack/react-table";
 import { css } from "@tailor-platform/styled-system/css";
 import { MoreVertical as MoreVerticalIcon } from "lucide-react";
-import { createPortal } from "react-dom";
 import { Button } from "../../../Button";
 import { addEventOutside } from "../addEventOutside";
 import type { Localization } from "@locales/types";
-
-const ModalPortal = ({ children }: { children: React.ReactNode }) => {
-  const el = document.createElement("div");
-  el.id = "pinned-column-modal";
-  document.body.appendChild(el);
-
-  useEffect(() => {
-    return () => {
-      document.body.removeChild(el);
-    };
-  }, [el]);
-
-  return createPortal(children, el);
-};
 
 type PinnedColumnProps<TData extends Record<string, unknown>> = {
   column: Column<TData>;
@@ -43,8 +28,9 @@ export const PinnedColumn = <TData extends Record<string, unknown>>({
       return {};
     }
     return {
-      top: Math.ceil(window.scrollY + box.bottom),
-      left: Math.ceil(window.scrollX + box.left - 100), // adjusted to display just below the MoreVerticalIcon
+      position: "fixed",
+      top: Math.ceil(box.bottom),
+      left: Math.ceil(box.left - 100), // adjusted to display just below the MoreVerticalIcon
     };
   };
 
@@ -72,61 +58,58 @@ export const PinnedColumn = <TData extends Record<string, unknown>>({
         />
       </Box>
       {isOpenedPinnedColumnModal && (
-        <ModalPortal>
-          <Box
-            pl={4}
-            pt={4}
-            w="140px"
-            h="88px"
-            borderRadius={"4px"}
-            boxShadow="lg"
-            position={"absolute"}
-            backgroundColor={"bg.default"}
-            display={"flex"}
-            flexDirection={"column"}
-            alignItems={"start"}
-            right={column.getIsPinned() === "left" ? "auto" : 0}
-            ref={modalRef}
-            style={{
-              ...getBoxPosition(),
+        <Box
+          pl={4}
+          pt={4}
+          w="140px"
+          h="88px"
+          borderRadius={"4px"}
+          boxShadow="lg"
+          backgroundColor={"bg.default"}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"start"}
+          right={column.getIsPinned() === "left" ? "auto" : 0}
+          ref={modalRef}
+          style={{
+            ...getBoxPosition(),
+          }}
+        >
+          <Button
+            variant="link"
+            size="xs"
+            mb={4}
+            onClick={() => {
+              if (column.getIsPinned() === "right") {
+                column.pin(false);
+              } else {
+                column.pin("right");
+              }
+              setIsOpenedPinnedColumnModal(false);
             }}
           >
-            <Button
-              variant="link"
-              size="xs"
-              mb={4}
-              onClick={() => {
-                if (column.getIsPinned() === "right") {
-                  column.pin(false);
-                } else {
-                  column.pin("right");
-                }
-                setIsOpenedPinnedColumnModal(false);
-              }}
-            >
-              {column.getIsPinned() === "right"
-                ? localization.columnFeatures.pinnedColumn.unPin
-                : localization.columnFeatures.pinnedColumn.pinnedRight}
-            </Button>
-            <Button
-              variant="link"
-              size="xs"
-              mb={4}
-              onClick={() => {
-                if (column.getIsPinned() === "left") {
-                  column.pin(false);
-                } else {
-                  column.pin("left");
-                }
-                setIsOpenedPinnedColumnModal(false);
-              }}
-            >
-              {column.getIsPinned() === "left"
-                ? localization.columnFeatures.pinnedColumn.unPin
-                : localization.columnFeatures.pinnedColumn.pinnedLeft}
-            </Button>
-          </Box>
-        </ModalPortal>
+            {column.getIsPinned() === "right"
+              ? localization.columnFeatures.pinnedColumn.unPin
+              : localization.columnFeatures.pinnedColumn.pinnedRight}
+          </Button>
+          <Button
+            variant="link"
+            size="xs"
+            mb={4}
+            onClick={() => {
+              if (column.getIsPinned() === "left") {
+                column.pin(false);
+              } else {
+                column.pin("left");
+              }
+              setIsOpenedPinnedColumnModal(false);
+            }}
+          >
+            {column.getIsPinned() === "left"
+              ? localization.columnFeatures.pinnedColumn.unPin
+              : localization.columnFeatures.pinnedColumn.pinnedLeft}
+          </Button>
+        </Box>
       )}
     </div>
   );
