f71f3b0998bace9a8cb7da6d2840d81f31401a98
IzumiSy <beetle-noise@gmx.com>
2024-05-21 14:24:52 +0900

    chore(ds): add vitest-fail-on-console in ds tests

--- packages/design-systems/tests/setup.ts
+++ packages/design-systems/tests/setup.ts
@@ -1,6 +1,8 @@
 import "@testing-library/jest-dom/vitest";
+import failOnConsole from "vitest-fail-on-console";
 import { beforeEach } from "vitest";
 
+failOnConsole();
 beforeEach(() => {
   process.env.TZ = "Asia/Tokyo";
 });
