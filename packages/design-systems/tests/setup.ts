007d363d88230a599e7af97224de8ec187bfa87f
IzumiSy <beetle-noise@gmx.com>
2024-05-21 15:36:47 +0900

    chore(ds): add global setup again

--- packages/design-systems/tests/setup.ts
+++ packages/design-systems/tests/setup.ts
@@ -2,10 +2,3 @@ import "@testing-library/jest-dom/vitest";
 import failOnConsole from "vitest-fail-on-console";
 
 failOnConsole();
-
-process.env.TZ = "Asia/Tokyo";
-/*
-beforeEach(() => {
-
-});
-*/
