895d069f66815835d3a4b7a8b964479edf6bb716
IzumiSy <beetle-noise@gmx.com>
2024-06-13 16:07:19 +0900

    chore(ds): add more types for tests

--- packages/design-systems/src/components/composite/Datagrid/SearchFilter/query/builder.test.ts
+++ packages/design-systems/src/components/composite/Datagrid/SearchFilter/query/builder.test.ts
@@ -21,6 +21,41 @@ describe("filterQuery", () => {
         type: "boolean",
       },
     },
+    {
+      accessorKey: "groupID",
+      meta: {
+        type: "uuid",
+      },
+    },
+    {
+      accessorKey: "category",
+      meta: {
+        type: "enum",
+        enumType: {
+          CATEGORY1: "Category 1",
+          CATEGORY2: "Category 2",
+          CATEGORY3: "Category 3",
+        },
+      },
+    },
+    {
+      accessorKey: "birthday",
+      meta: {
+        type: "date",
+      },
+    },
+    {
+      accessorKey: "createdAt",
+      meta: {
+        type: "dateTime",
+      },
+    },
+    {
+      accessorKey: "workingTime",
+      meta: {
+        type: "time",
+      },
+    },
   ] as const;
 
   const { query, filterOp } = newQueryBuilder({
@@ -31,29 +66,60 @@ describe("filterQuery", () => {
     const q = query({
       // add extra spaces to test the trimming
       name: filterOp.string.eq("John  "),
+      age: filterOp.number.gt(10),
     }).and([
       query({
         hasJob: filterOp.boolean.eq(true),
+        groupID: filterOp.uuid.eq("b2c38958-feeb-4198-8b7b-14da2a67bce6"),
       }),
       query({
-        age: filterOp.number.gt(10),
-      }),
+        category: filterOp.enum.eq("CATEGORY1"),
+        birthday: filterOp.date.eq("2021-01-01"),
+      }).or([
+        query({
+          createdAt: filterOp.dateTime.eq("2021-01-01T00:00:00Z"),
+        }),
+        query({
+          workingTime: filterOp.time.eq("12:00:00"),
+        }),
+      ]),
     ]);
 
     expect(q.build()).toStrictEqual({
       name: {
         eq: "John",
       },
+      age: {
+        gt: 10,
+      },
       and: [
         {
           hasJob: {
             eq: true,
           },
+          groupID: {
+            eq: "b2c38958-feeb-4198-8b7b-14da2a67bce6",
+          },
         },
         {
-          age: {
-            gt: 10,
+          category: {
+            eq: "CATEGORY1",
           },
+          birthday: {
+            eq: "2021-01-01",
+          },
+          or: [
+            {
+              createdAt: {
+                eq: "2021-01-01T00:00:00Z",
+              },
+            },
+            {
+              workingTime: {
+                eq: "12:00:00",
+              },
+            },
+          ],
         },
       ],
     });
