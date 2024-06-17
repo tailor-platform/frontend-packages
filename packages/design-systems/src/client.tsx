b6002fd4251b6602899964eb0b607a2666a3ff30
IzumiSy <beetle-noise@gmx.com>
2024-06-17 14:09:10 +0900

    chore(ds): expose Columns type

--- packages/design-systems/src/client.tsx
+++ packages/design-systems/src/client.tsx
@@ -16,6 +16,7 @@ export {
   type GraphQLQueryFilter,
   type QueryRow,
   type Order,
+  type Columns,
   useDataGrid,
   newColumnBuilder,
   newFilterBuilder,
--- packages/design-systems/src/components/composite/Datagrid/index.tsx
+++ packages/design-systems/src/components/composite/Datagrid/index.tsx
@@ -8,5 +8,5 @@ export type {
   QueryRowValue,
 } from "./SearchFilter/types";
 export type { Localization } from "../../../locales/types";
-export { newColumnBuilder } from "./column";
+export { newColumnBuilder, type Columns } from "./column";
 export { newFilterBuilder } from "./SearchFilter/builder";
