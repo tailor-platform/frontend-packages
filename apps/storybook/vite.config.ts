5209d3e43a28bde0fd7a98024a1a969438acb7c1
onozeam <maezonoinfo@gmail.com>
2024-06-20 16:17:16 +0900

    chore(storybook: upgrade plugins

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,8 +1,10 @@
 import panda from "@pandacss/dev/postcss";
 import { defineConfig } from "vite";
 import * as path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [
