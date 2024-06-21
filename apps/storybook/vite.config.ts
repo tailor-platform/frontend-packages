05fdfbecec682c766057b130ed2cb0eea9446a57
onozeam <maezonoinfo@gmail.com>
2024-06-21 15:14:34 +0900

    fix: remove jsxruntime

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    react({
-      jsxRuntime: 'automatic',
-    }),
+    react(),
   ],
   css: {
     postcss: {
