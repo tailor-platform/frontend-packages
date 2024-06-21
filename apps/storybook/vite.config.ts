01c61185b3b8b70cbe00c5c79ce2dc0bee38e33b
onozeam <maezonoinfo@gmail.com>
2024-06-21 15:02:48 +0900

    fix: add vitejs/plugin-react

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,8 +1,14 @@
 import panda from "@pandacss/dev/postcss";
 import { defineConfig } from "vite";
 import * as path from "path";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
