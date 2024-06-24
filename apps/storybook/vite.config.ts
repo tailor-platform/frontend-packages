c6efe35a98a8d4fa043c9757b436611a8e131c8d
onozeam <maezonoinfo@gmail.com>
2024-06-24 13:40:04 +0900

    revert: execute `git revert 3ed9072feebef2707067c27dd3258807d396d71f`

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,10 +1,8 @@
 import panda from "@pandacss/dev/postcss";
 import { defineConfig } from "vite";
 import * as path from "path";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   css: {
     postcss: {
       plugins: [
