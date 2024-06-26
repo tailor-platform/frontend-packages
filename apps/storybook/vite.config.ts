c7bbaff02700a1873737c21988c962849afa652e
onozeam <maezonoinfo@gmail.com>
2024-06-26 16:50:48 +0900

    fix: use custom enviroment variables for `storybook dev`

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: process.env.CI ? [react()] : [],
+  plugins: process.env._STORYBOOK_DEV ? [] : [react()],
   css: {
     postcss: {
       plugins: [
